79b92838cd8a3b37cb588dc4639633dc57160c72
Leo <584395720@qq.com>
2020-07-23 14:01:03 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="app">
+    <h2>hellow word</h2>
   </div>
 </template>
 
-<style>
+
+<style lang="stylus" scoped>
+ @import '~assets/stylus/variable.styl'
+
+//  h2
+//   color:$color-theme
 
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    resolve:{ //配置快捷路径
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  },
 }
