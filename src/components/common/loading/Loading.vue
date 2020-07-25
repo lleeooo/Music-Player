df6bab29dbc16a91b278f4cc4e6fb1435f3ead3c
Leo <584395720@qq.com>
2020-07-25 15:16:49 +0800

    feat:增加数据未获取前的整体loding gif图显示

---
+++ src/components/common/loading/Loading.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="loading">
+    <img src="~assets/img/recommend/loading.gif" alt="">
+    <p>正在加载</p>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: "Loading"
+}
+</script>
+
+<style lang="stylus" scoped>
+.loading
+  text-align center
+
+  img
+    width 25px
+    height 25px
+  p
+    font-size 10px
+    padding-top 5px
+
+
+</style>
--- src/views/recommend/Recommend.vue
+++ src/views/recommend/Recommend.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="recommend">
-
     <scroll class="content" ref="scroll">
       <recommend-swiper />
       <div class="text">热门歌单推荐</div>
-      <recommend-hotlist :hotList="hostList" @HotistImgload="HotistImgload"/>
+      <recommend-hotlist :hotList="hostList" @HotistImgload="HotistImgload" class="hot-list" />
+      <loading class="loading" v-show="!hostList.length"></loading>
     </scroll>
-
   </div>
 </template>
 
 <script>
 //better-scroll
 import scroll from "components/common/scroll/Scroll";
+import loading from "components/common/loading/Loading";
 
 //子组件
 import RecommendSwiper from "./recommendChild/RecommendSwipe";
@@ -30,6 +30,7 @@ export default {
   },
   components: {
     scroll,
+    loading,
 
     RecommendSwiper,
     RecommendHotlist,
@@ -41,17 +42,16 @@ export default {
     //网络请求相关方法
     _getHotList() {
       getHotList().then((res) => {
-        this.hostList = res.data.playlists;
+          this.hostList = res.data.playlists;
       });
     },
 
     //获取数据相关方法
     //每一次获取到照片 就刷新一次scroll的高度
-    HotistImgload(){
-      this.$refs.scroll.refresh()
-      console.log(1)
-    }
-
+    HotistImgload() {
+      this.$refs.scroll.refresh();
+      console.log(1);
+    },
   },
 };
 </script>
@@ -65,14 +65,21 @@ export default {
   .content {
     height: calc(100% - 88px);
     overflow: hidden;
-  }
+    position relative
 
-  .text {
-    text-align: center;
-    color: $color-theme;
-    font-size: $font-size-medium;
-    height: 65px;
-    line-height: 65px;
+    .text {
+      text-align: center;
+      color: $color-theme;
+      font-size: $font-size-medium;
+      height: 65px;
+      line-height: 65px;
+    }
+    .loading {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
   }
 }
 </style>
